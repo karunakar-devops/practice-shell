766c94ac34c5552b2fb20e7f66f4356d93498b6a
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-20 15:15:12 +0530

    credentials

--- 05-usercredentials.sh
+++ 05-usercredentials.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter username:"
 
-read -n USERNAME
+read -s USERNAME
 
 echo "Please enter password:"
 
-read -n PASSWORD
+read -s PASSWORD
 
 echo "username: $USERNAME and password: $PASSWORD"
