709d82fe7ff0a1df44edcd34f0feba59116e0853
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-20 14:43:54 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,5 +2,5 @@
 
 echo "Mahesh:: Hi Shiva, How are you?"
 echo "Shiva:: Hi Mahesh, I am doing good, how are you?"
-echo "Mahesh:: I am Good, How you are work going on?"
-echo "Shiva:: not bad, i am learning Devops"
+echo "Mahesh:: I am Good, How is your work going on?"
+echo "Shiva:: not bad, i am updating to  Devops"
