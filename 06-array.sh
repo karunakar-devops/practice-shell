12b46bac93caf21bef877a3e87a21f318ddcded3
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-20 15:22:10 +0530

    arrays

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Fruits=("Apple" "Orange" "Mango" "Bannana" "Grapes")
+
+echo "First Fruit is:: {$Fruits[0]}
+echo "Second Fruit is:: {$Fruits[1]}
+echo "Third Fruit is:: {$Fruits[$]}
+echo "Fourth Fruit is:: {$Fruits[3]}
+echo "Fifth Fruit is:: {$Fruits[4]}
+echo "sixth Fruit is:: {$Fruits[5]}
+echo " Fruits are:: {$Fruits[$@]}
