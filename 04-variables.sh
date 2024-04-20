fa2815d8dcc81744e3fe121d83cc408cdb054758
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-20 14:55:28 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$1:: Hi $2, How are you?"
+echo "{$2}:: Hi $1, I am doing good, how are you?"
+echo "$$1:: I am Good, How is your work going on?"
+echo "${2}:: not bad, i am updating to  Devops"
