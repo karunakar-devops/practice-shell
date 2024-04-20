31b7ffe0ed472b9686b4148542b90e82791b70f7
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-20 15:52:41 +0530

    arrays

--- 06-array.sh
+++ 06-array.sh
@@ -2,10 +2,10 @@
 
 Fruits=("Apple" "Orange" "Mango" "Bannana" "Grapes")
 
-echo "First Fruit is:: $Fruits[0]"
-echo "Second Fruit is:: $Fruits[1]"
-echo "Third Fruit is:: $Fruits[2]"
-echo "Fourth Fruit is:: $Fruits[3]"
-echo "Fifth Fruit is:: $Fruits[4]"
-echo "sixth Fruit is:: $Fruits[5]"
-echo " Fruits are:: $Fruits [@]"
+echo "First Fruit is:: ${Fruits[0]}"
+echo "Second Fruit is:: ${Fruits[1]}"
+echo "Third Fruit is:: ${Fruits[2]}"
+echo "Fourth Fruit is:: ${Fruits[3]}"
+echo "Fifth Fruit is:: ${Fruits[4]}"
+echo "sixth Fruit is:: ${Fruits[5]}"
+echo " Fruits are:: ${Fruits [@]}"
