08b0466ff17b24c4691d64413d93b534e8a74358
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-22 15:23:21 +0530

    installations

--- 10-installation.sh
+++ 10-installation.sh
@@ -14,3 +14,18 @@ fi
 dnf install mysql -y
 
 echo "is still proceeding?"
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run with root access"
+    exit 12
+    
+else
+    echo "you are root user"
+fi
+
+dnf install git -y
+
+
+
+
