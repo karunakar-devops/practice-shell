4aa01755e6a62ba90e17fa4037a3de72ddce674f
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-22 13:13:58 +0530

    special vars

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-#NUMBER = $1
-
-#if [ $NUMBER -gt 10 ]
-#then
-  #  echo "Given Number $NUMBER is greater than 10"
-#else
- #   echo "Given Number $NUMBER is less than 10"
-#fi
-
-
-NUMBER=$1
+NUMBER = $1
 
 if [ $NUMBER -gt 10 ]
 then
-    echo "Given Number $NUMBER is greater than 10"
-else 
-    echo "Given Number $NUMBER is less than 10"
+   echo "Given Number $NUMBER is greater than 10"
+else
+  echo "Given Number $NUMBER is less than 10"
 fi
+
+
