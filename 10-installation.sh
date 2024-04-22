66bd005b30ed9539a990632678b7fd22cc1da391
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-22 15:36:50 +0530

    installations

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,41 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
+    USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run with root access"
-    exit 12
-    
-else
-    echo "you are root user"
-fi
+    if [ $USERID -e 0 ]
+    then 
+    echo "running with root user"
 
+    else 
+    echo "please run with super user"
+    if
 dnf install mysql -y
-
-#echo "is still proceeding?"
-
-if [ $? -ne 0 ]
-then
-    echo "installtion of mysql....failure"
-    exit 1
-    
-else
-    echo "installation of mysql....sucess"
-fi
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "installtion of git....failure"
-    exit 1
-    
-else
-    echo "installation of git....sucess"
-fi
-
-
 
 
 
