962f938725cb7feb5617ee3660670e8d43ddaa34
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-22 15:07:13 +0530

    installations

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,8 +5,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run with root access"
+    
 else
     echo "you are root user"
 fi
 
 dnf install mysql -y
+
+echo "is still proceeding?"
