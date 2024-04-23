bbb33a6133616c2b4d35fcd9f972095d1694df75
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-23 15:35:23 +0530

    install packages

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -32,7 +32,7 @@ for i in $@
 do
 echo "package to install :$i"
 dnf list installed $i &>>$LOGFILE
-if [ $1 -eq 0 ]
+if [ $? -eq 0 ]
 then
     echo -e "$i already installed......$Y SKIPPING $N"
 else 
