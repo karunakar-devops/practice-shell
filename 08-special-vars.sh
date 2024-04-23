ca7a3ea9e29000c6d9592c244aa3c836f260fbb0
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-23 11:55:51 +0530

    Adding colors to script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,7 +5,7 @@ echo "No of variables Passed:$#"
 echo "Name of script running:$0"
 echo "present working directory:$PWD"
 echo "Home Directory:$HOME"
-echo "Exit ststus of last command:$?"
+echo "Exit status of last command:$?"
 echo "which user running this script:$USER"
 echo "process id of the current shell script:$$"
 Sleep 60&
---
+++ 12-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+ 
+USERID=$(id -u)
+TIMESTAMP=(date +%F-%H-%M-%S)
+SCRIPT_NAME=(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run with root user"
+    exit 1
+else    
+    echo "You are super user"
+fi
+
+VALIDATE(){
+if [ $1 -ne 0]
+then
+    echo -e $R "$2.....failure $N"
+    exit 12
+else
+    echo -e $G"$2....success $N"
+
+}
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $?, "Installing Mysql"
+
+dnf install git -y &>>LOGFILE
+VALIDATE $?, "Installing Git"
+
+
+
+
+
+
