7973b8b547db12d8b6b76aad0ee3bdee188aa301
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-23 14:51:06 +0530

    install packages

---
+++ 14-install-packages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R=\e["31m"
+G=\e["32m"
+Y=\e["33m"
+N=\e["0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2..... $R failure $N"
+    exit 1
+else
+    echo -e "$2..... $G success $N"
+fi
+}
+
+for i in $@
+do
+echo "package to install :$i"
+dnf list installed $i &>>$LOGFILE
+if [ $1 -eq 0 ]
+then
+    echo -e "$i already installed......$Y SKIPPING $N"
+else 
+     dnf install $i -y &>>$LOGFILE
+    VALIDATE $? "Installation of $i"
+done
