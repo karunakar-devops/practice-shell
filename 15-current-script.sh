3a6c91c6064a03993389789d9b1cd136620127dc
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-27 19:53:57 +0530

    calling script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,9 @@ COURSE="Devops from Current Script"
 echo "Before calling other script, course=$COURSE"
 echo "Process Id of current shell script:$$"
 
-./16-other-script.sh
+#./16-other-script.sh
+
+source ./16-other-script.sh
 
 echo "After calling other script, course=$COURSE"
 
