4c40c1befbe0f6dbf058ffda0194c68a96d6d606
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-27 20:12:07 +0530

    set command

--- 17-set.sh
+++ 17-set.sh
@@ -4,21 +4,13 @@ set -e
 
 USERID=$(id -u)
 
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access"
+    exit 1 
 else
     echo "You are super user"
 fi
-
-VALIDATE(){
-if [ $? -ne 0 ]
-then
-    echo "installation of mysql.......Failure"
-else
-    echo "installation of mysql......success"
-fi
-}
 
 dnf install mysqleee-server -y 
 
